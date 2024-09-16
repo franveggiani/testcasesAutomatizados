6de87322b37980cf8584d104b20ada15be2a44f7
Cristian Suarez <csuarez@carconnect.com.ec>
2024-09-15 22:19:18 -0300

    fix test data

--- cypress/e2e/csuarez/create-user.cy.js
+++ cypress/e2e/csuarez/create-user.cy.js
@@ -1,6 +1,7 @@
-describe("When create user user case is called", () => {
+describe("When create user use case is called", () => {
 	it("Should not create a user with an existing email", () => {
-		cy.intercept("POST", "/users", {
+		// Request fail mock
+		cy.intercept("POST", "https://thinking-tester-contact-list.herokuapp.com/users", {
 			statusCode: 400,
 			body: {
 			  message: "Email address is already in use",
@@ -9,13 +10,13 @@ describe("When create user user case is called", () => {
 	  
 		  cy.visit("https://thinking-tester-contact-list.herokuapp.com/addUser");
 	  
-		  cy.get('input[id="firstName"]').type("Cristian");
-		  cy.get('input[id="lastName"]').type("Suarez");
+		  cy.get('input[id="firstName"]').type("testing-name");
+		  cy.get('input[id="lastName"]').type("testing-lastname");
 		  cy.get('input[id="email"]').type("existingemail@example.com");
 		  cy.get('input[id="password"]').type("test1234");
 		  cy.get('button[type="submit"]').click();
 	  
-		  cy.wait("@createUser");
+		  cy.wait("@createUser", { timeout: 10000 });
 		  cy.contains("Email address is already in use").should("be.visible");
 	});
 });
